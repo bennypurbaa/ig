bf2582ebb4e8886c858b0a5d8384e19cf4c632e4
bennypurbaa <34009729+bennypurbaa@users.noreply.github.com>
2017-11-27 04:40:25 +0700

    Update fftauto.js

--- fftauto.js
+++ fftauto.js
@@ -12,8 +12,8 @@ const rp = require('request-promise');
 const _ = require('lodash');
 
 const User = {
-    username: '',
-    password: ''
+    username: 'bennypurbaa',
+    password: 'sibenny18'
 }
 
 const TargetUsername = '';
